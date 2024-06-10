e82b101fb4592e7ac6a7e3456ae7c66ecf303c42
Madhav <madhavpanchal1716@gmail.com>
2024-06-10 23:14:54 +0530

    Adsense

--- src/app/components/Adsense.jsx
+++ src/app/components/Adsense.jsx
@@ -2,21 +2,14 @@ import Script from "next/script";
 
 const AdSense = () => {
   return (
-    <div>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-YOUR_ADSENSE_CLIENT_ID"
-        data-ad-slot="YOUR_ADSENSE_AD_SLOT"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
+    <>
       <Script
         async
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9168362458076214`}
         strategy="afterInteractive"
         crossOrigin="anonymous"
       />
-    </div>
+    </>
   );
 };
 
